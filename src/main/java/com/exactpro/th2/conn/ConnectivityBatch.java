23361a4eb04e663c9b62064365d6c96bf5b047d1
Andrey Drobynin <andrey.drobynin@exactprosystems.com>
2022-02-14 13:14:49 +0400

    code review: extracted method checkForUnorderedSequences()

--- src/main/java/com/exactpro/th2/conn/ConnectivityBatch.java
+++ src/main/java/com/exactpro/th2/conn/ConnectivityBatch.java
@@ -85,34 +85,42 @@ public class ConnectivityBatch {
         }
 
         if (LOGGER.isErrorEnabled()) {
-            boolean sequencesUnordered = false;
-            List<Long> missedSequences = new ArrayList<>();
-            for (int index = 0; index < iMessages.size() - 1; index++) {
-                long nextExpectedSequence = iMessages.get(index).getSequence() + 1;
-                long nextSequence = iMessages.get(index + 1).getSequence();
-                if (nextExpectedSequence != nextSequence) {
-                    sequencesUnordered = true;
-                }
-                LongStream.range(nextExpectedSequence, nextSequence).forEach(missedSequences::add);
-            }
-            if (sequencesUnordered) {
-                LOGGER.error(
-                        "List {} hasn't elements with incremental sequence with one step for session alias '{}' and direction '{}'{}",
-                        iMessages.stream()
-                                .map(ConnectivityMessage::getSequence)
-                                .collect(Collectors.toList()),
-                        sessionAlias,
-                        direction,
-                        missedSequences.isEmpty()
-                                ? ""
-                                : String.format(". Missed sequences %s", missedSequences)
-                );
-            }
+            checkForUnorderedSequences(iMessages, sessionAlias, direction);
         }
 
             // FIXME: Replace logging to thowing exception after solving message reordering problem
 //            throw new IllegalArgumentException("List " + iMessages.stream()
 //                    .map(ConnectivityMessage::getSequence)
 //                    .collect(Collectors.toList())+ " hasn't elements with incremental sequence with one step");
+    }
+
+    private static void checkForUnorderedSequences(
+            List<ConnectivityMessage> iMessages,
+            String sessionAlias,
+            Direction direction
+    ) {
+        boolean sequencesUnordered = false;
+        List<Long> missedSequences = new ArrayList<>();
+        for (int index = 0; index < iMessages.size() - 1; index++) {
+            long nextExpectedSequence = iMessages.get(index).getSequence() + 1;
+            long nextSequence = iMessages.get(index + 1).getSequence();
+            if (nextExpectedSequence != nextSequence) {
+                sequencesUnordered = true;
+                LongStream.range(nextExpectedSequence, nextSequence).forEach(missedSequences::add);
+            }
+        }
+        if (sequencesUnordered) {
+            LOGGER.error(
+                    "List {} hasn't elements with incremental sequence with one step for session alias '{}' and direction '{}'{}",
+                    iMessages.stream()
+                            .map(ConnectivityMessage::getSequence)
+                            .collect(Collectors.toList()),
+                    sessionAlias,
+                    direction,
+                    missedSequences.isEmpty()
+                            ? ""
+                            : String.format(". Missed sequences %s", missedSequences)
+            );
+        }
     }
 }
